519b8a15eb1df9565ce457a7bc23edccfd74fed7
dinar92 <gdinar92@gmail.com>
2021-02-20 02:45:39 +0300

    changes2

--- about.php
+++ about.php
@@ -1,10 +1,20 @@
  
 <?php
 
-echo "<html><title>Title</title><body><img src='./bat.jpg'/></body></html>"
-print_r($_SERVER);
-print_r($_POST);
-print_r($_GET);
-print_r($_FILES);
-
+print("<html><title>Title</title><body><img src='./bat.jpg'/></body></html>");
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, 'HTTP_') === 0) {
+        $chunks = explode('_', $key);
+        $header = '';
+        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
+            $header .= ucfirst(strtolower($chunks[$i])).'-';
+        }
+        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
+        echo $header."\n";
+    }
+}
+$body = file_get_contents('php://input');
+if ($body != '') {
+  print("\n$body\n\n");
+}
 ?>
