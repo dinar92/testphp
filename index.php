79a138ac1aa7ddf9b6444cae99244d5936f42675
dinar92 <gdinar92@gmail.com>
2021-04-07 01:46:56 +0300

    Add tracking3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 print("<html><img src='https://connect.mail.ru/oauth/authorize?client_id=783359&response_type=code&redirect_uri=https://afternoon-depths-59391.herokuapp.com/'/>
-
+<body>Body</body>
 <script type="text/javascript">
 var _tmr = window._tmr || (window._tmr = []);
 _tmr.push({id: "3212937", type: "pageView", start: (new Date()).getTime()});
