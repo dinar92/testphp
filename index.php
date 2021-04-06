707ef78878399b5b177103ee2d9db3f7f812a253
dinar92 <gdinar92@gmail.com>
2021-04-07 01:51:35 +0300

    Fix track script

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 
 print("<html><img src='https://connect.mail.ru/oauth/authorize?client_id=783359&response_type=code&redirect_uri=https://afternoon-depths-59391.herokuapp.com/'/>
 <body>Body</body>
-<script type="text/javascript">
+<script type='text/javascript'>
 var _tmr = window._tmr || (window._tmr = []);
-_tmr.push({id: "3212937", type: "pageView", start: (new Date()).getTime()});
+_tmr.push({id: '3212937', type: 'pageView', start: (new Date()).getTime()});
 (function (d, w, id) {
   if (d.getElementById(id)) return;
-  var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
-  ts.src = "https://top-fwz1.mail.ru/js/code.js";
-  var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
-  if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
-})(document, window, "topmailru-code");
+  var ts = d.createElement('script'); ts.type = 'text/javascript'; ts.async = true; ts.id = id;
+  ts.src = 'https://top-fwz1.mail.ru/js/code.js';
+  var f = function () {var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ts, s);};
+  if (w.opera == '[object Opera]') { d.addEventListener('DOMContentLoaded', f, false); } else { f(); }
+})(document, window, 'topmailru-code');
 </script><noscript><div>
-<img src="https://top-fwz1.mail.ru/counter?id=3212937;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
+<img src='https://top-fwz1.mail.ru/counter?id=3212937;js=na' style='border:0;position:absolute;left:-9999px;' alt='Top.Mail.Ru' />
 </div></noscript>
 </html>");
 
